00415124106710bdef71af5835a6d8369b90e36c
Hadi Kashlan <hadi.kashlan@akelius.de>
2024-04-09 08:00:00 +0200

    add Post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PostModule } from './api/post/post.module';
 import { UserModule } from './api/user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
