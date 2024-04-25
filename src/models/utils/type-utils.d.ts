ecc1890f867ebf3f809c2c38086bc0011993520f
Hadi Kashlan <hadi.kashlan@akelius.de>
2024-04-25 18:58:25 +0200

    fix types

--- src/models/utils/type-utils.d.ts
+++ src/models/utils/type-utils.d.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export type PropType<T> = Omit<
   T,
   | 'id'
@@ -8,5 +10,22 @@ export type PropType<T> = Omit<
   | 'updatedUserName'
   | 'updatedUserId'
 >;
+type IsSimpleType<T> = T extends string | number | boolean | symbol | Enum
+  ? true
+  : false;
 
-export type ZodOutputFor<T> = z.ZodType<PropType<T>, z.ZodTypeDef, unknown>;
+interface Enum {
+  // Marker interface for enums
+}
+
+type IsObject<T> = T extends object
+  ? T extends null | undefined
+    ? never
+    : true
+  : never;
+
+type SimpleType<T> = {
+  [P in keyof T as IsSimpleType<T[P]> extends true ? P : never]: T[P];
+};
+
+export type ZodOutputFor<T> = z.ZodType<SimpleType<PropType<T>>>;
