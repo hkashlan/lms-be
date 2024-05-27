3f0dd0d3fb45082d2e18fd2179a9106c885a4ee8
Hadi Kashlan <hadi.kashlan@akelius.de>
2024-05-27 05:49:13 +0200

    add student information skelaton

--- src/api/student/student.controller.ts
+++ src/api/student/student.controller.ts
@@ -1,9 +1,32 @@
-import { Controller } from '@nestjs/common';
-import { Student, Prisma } from '@prisma/client';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Course, CourseInstance, PathInstance, Prisma, QuizInstance, Role, Student } from '@prisma/client';
+import { of } from 'rxjs';
+import { Payload } from '../../auth/auth.service';
+import { CurrentUser, Public } from '../../auth/constants';
 import { ModelRestController } from '../../core/api/model.controller';
 import { StudentValidation } from '../../models/validation/student.z';
 import { StudentService } from './student.service';
 
+export interface StudentInfo {
+  courses: CourseInstance[];
+  quizzes: QuizInstance[];
+  pathes: PathInstance[];
+}
+
+export interface FinishStudentLesson {
+  done?: boolean;
+  fullMark?: number;
+  mark?: number;
+  answeredOptions?: number[][];
+  courseId: number;
+  lessonId: number;
+}
+export interface FinishStudentQuiz {
+  fullMark: number;
+  mark: number;
+  answeredOptions: number[][];
+  quizzId: number;
+}
 
 @Controller('student')
 export class StudentController extends ModelRestController<
@@ -14,5 +37,53 @@ export class StudentController extends ModelRestController<
 > {
   constructor(studentService: StudentService) {
     super(studentService, StudentValidation);
+  }
+
+  @Public()
+  @Get('my-paths')
+  async getMyPaths(@CurrentUser() user: Payload) {
+    user = this.fakeUser(user);
+    console.log('user', user);
+    return of<StudentInfo>({
+      courses: [],
+      quizzes: [],
+      pathes: [],
+    });
+  }
+
+  @Public()
+  @Get('open-paths')
+  async getOpenPaths(@CurrentUser() user: Payload) {
+    user = this.fakeUser(user);
+    console.log('user', user);
+    return of<PathInstance[]>([]);
+  }
+
+  @Public()
+  @Post('finish-exam')
+  async finishExam(@Body() createUserDto: FinishStudentQuiz, @CurrentUser() user: Payload) {
+    user = this.fakeUser(user);
+    console.log('user', user);
+    console.log('createUserDto', createUserDto);
+    return of<QuizInstance>({} as QuizInstance);
+  }
+
+  @Public()
+  @Post('finish-lesson')
+  async finishLesson(@Body() createUserDto: FinishStudentLesson, @CurrentUser() user: Payload) {
+    user = this.fakeUser(user);
+    console.log('user', user);
+    console.log('createUserDto', createUserDto);
+    return of<Course>({} as Course);
+  }
+
+  private fakeUser(user: Payload) {
+    user = {
+      sub: 1,
+      username: 'test',
+      name: 'test',
+      roles: [Role.STUDENT],
+    };
+    return user;
   }
 }
